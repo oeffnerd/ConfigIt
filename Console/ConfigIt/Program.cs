4fc8efd6ca668008db6cc2191cd3f7ab8331ed89
David <doeffner3@gmail.com>
2019-07-28 12:45:00 -0400

    Root

---
+++ Console/ConfigIt/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConfigIt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            listFilesInDirectory(@"C:\Users\David\Desktop\testdata\input");
+
+            Console.WriteLine("Press Enter to Continue");
+            Console.Read();
+        }
+
+        static void listFilesInDirectory (string workingDirectory)
+        {
+            string[] filePaths = Directory.GetFiles(workingDirectory);
+            foreach(string filePath in filePaths)
+            {
+                Console.WriteLine(filePath);
+            }
+        }
+    }
+}
